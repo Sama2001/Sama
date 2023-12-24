839f3650787f8c32654fa6b09b6d53bdfb5a50c8
Sama2001 <samafadah2001@gmail.com>
2023-12-23 22:25:05 -0800

    new

--- tictactoe.dart
+++ tictactoe.dart
@@ -1,37 +1,37 @@
 import 'dart:io';
 
-List<String> board = List.generate(9, (index) => (index + 1).toString());
+List<String> board = List.generate(9, (index) => (index + 1).toString()); ///1-9
 enum players1 {X,O} 
 void main() {
 
-  int currentPlayer = 1; 
+  int player = 1;  ///initial index for current player
 
   while (true) {
-    printBoard();
+    printing(); ///print the board
 
     
-    int move = getPlayerMove(currentPlayer);
+    int move = getMove(player);
 
     
-    updateBoard(move, currentPlayer);
+    update(move, player); ///update the board
 
     
-    if (checkWinner(currentPlayer)) {
-      printBoard();
-      print("Player $currentPlayer wins!");
+    if (WinnerChecker(player)) {
+      printing();
+      print("Player ${player == 1 ? 'X' : 'O'} wins ^_^");
       break;
-    } else if (board.every((cell) => cell == 'X' || cell == 'O')) {
-      printBoard();
-      print("It's a draw!");
+    } else if (board.every((cell) => cell == 'X' || cell == 'O')) 
+    {
+      printing(); ///print the board
       break;
     }
 
-    currentPlayer = 3 - currentPlayer;
+    player = 3 - player;
   }
 }
 
-
-void printBoard() {
+//////////////board printing///////////////////////
+void printing() {
   print(" ${board[0]} | ${board[1]} | ${board[2]} ");
   print("---+---+---");
   print(" ${board[3]} | ${board[4]} | ${board[5]} ");
@@ -39,32 +39,34 @@ void printBoard() {
   print(" ${board[6]} | ${board[7]} | ${board[8]} ");
 }
 
-int getPlayerMove(int currentPlayer) {
+/////////////////////get moves for current player///////////
+int getMove(int player) {
   int move;
   while (true) {
 
-    stdout.write("Player $currentPlayer, please enter the number of the square where you want to place your ${currentPlayer == 1 ? 'X' : 'O'} : ");
+    stdout.write("Player $player, please enter the number of the square where you want to place your ${player == 1 ? 'X' : 'O'} : ");
     String input = stdin.readLineSync() ?? "";
     try {
       move = int.parse(input);
-      if (move >= 1 && move <= 9 && board[move - 1] == move.toString()) {
+      if (move >= 1 && move <= 9 && board[move - 1] == move.toString()) {  /// enterd values (inputs) 1-9 
         break;
       } else {
-        print("Invalid move. Please choose a number between 1 and 9.");
+        print("wrong input, choose a number between 1 and 9.");
       }
-    } catch (e) {
-      print("Invalid input. Please enter a number between 1 and 9.");
+    } 
+    catch (e) {
+      print("wrong input, choose a number between 1 and 9.");
     }
   }
   return move;
 }
 
-void updateBoard(int move, int currentPlayer) {
-  board[move - 1] = (currentPlayer == 1) ? 'X' : 'O';
+void update(int move, int player) {
+  board[move - 1] = (player == 1) ? 'X' : 'O';
 }
-
-bool checkWinner(int currentPlayer) {
-  List<List<int>> winPatterns = [
+/////////////////winner check////////
+bool WinnerChecker(int player) {
+  List<List<int>> win = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -75,10 +77,10 @@ bool checkWinner(int currentPlayer) {
     [2, 4, 6],
   ];
 
-  for (var pattern in winPatterns) {
+  for (var pattern in win) {
     if (board[pattern[0]] == board[pattern[1]] &&
         board[pattern[1]] == board[pattern[2]] &&
-        board[pattern[0]] == (currentPlayer == 1 ? 'X' : 'O')) {
+        board[pattern[0]] == (player == 1 ? 'X' : 'O')) {
       return true;
     }
   }
